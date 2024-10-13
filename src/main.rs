7b3fdaf1720ee2f3631939bd788b494b933f2a6e
KartalToth240560 <240560@buas.nl>
2024-10-13 15:57:52 +0200

    Working parser

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,11 @@ struct Lexer<'a> {
     index: usize,
 }
 impl<'a> Lexer<'a> {
-    fn token_from(&self, state: State, from_to: usize) -> Option<Token<'a>> {
-        return match state {
-            State::Unknown => Some(Token::Eof),
-            State::Integer => {
-                let parsed = &self.get(from_to).parse::<i32>();
-                println!("{:?}", parsed);
-                match parsed{
-                    Ok(num) => Some(Token::Integer(*num)),
-                    Err(e) => panic!("{:?}", e)
-                }
-                
-            },
-            State::Float => Some(Token::Float(self.get(from_to).parse().unwrap())),
-            State::String => panic!("Unfinished string!"),
-            State::Identifier => todo!()
-        };
+    fn character_token(&mut self, character: char, token_type: Token<'a>) -> Option<Token<'a>>{
+        self.index += character.len_utf8();
+        return Some(token_type);
     }
+    
     pub fn new(input: &'a str) -> Lexer<'a>
     {
         Lexer {
@@ -31,9 +19,7 @@ impl<'a> Lexer<'a> {
             index: 0
         }
     }
-    fn get(&self, from_to: usize) -> &'a str {
-        &self.input[from_to..self.index]
-    }
+    
 }
 #[derive(PartialEq, Debug)]
 enum Token<'a> {
@@ -42,6 +28,10 @@ enum Token<'a> {
     String(&'a str),
     Integer(i32),
     Float(f32),
+    Plus,
+    Minus,
+    Increment,
+    Decrement,
     Eof
 }
 
@@ -50,8 +40,10 @@ enum State {
     Unknown,
     Integer,
     Float,
-    String,
-    Identifier
+    StartString,
+    EndString,
+    Identifier,
+    Operator
 }
 
 impl<'a> Iterator for Lexer<'a> {
@@ -62,74 +54,118 @@ impl<'a> Iterator for Lexer<'a> {
         let mut characters = self.input[self.index..].chars();
         let mut from_to = self.index;
         let mut state = State::Unknown;
+
         // Search token bounds.
         loop {
-            let start_index = self.index;
-            println!("{:?}", state);
             let Some(character) = characters.next() else {
-                return self.token_from(state, from_to);
+                break;
             };
+
+            let next_index: usize = self.index + character.len_utf8();
             
-            self.index += character.len_utf8();
-            
-            println!("{}", &self.input[from_to..self.index]);
+            //println!("State: {:?} Current: \"{}\" Character: \'{}\'", state, &self.input[from_to..next_index], character);
             if character.is_whitespace()  {
                 if state == State::Unknown {
-                    from_to = self.index;
+                    self.index = next_index;
                     continue;
                 }
                 else {
-                    self.index = start_index;
-                    return self.token_from(state, from_to);
+                    break;
                 }
             }
             
             match state {
                 State::Unknown => {
+                    from_to = self.index;
+                    // Extremely hacky solution for single character tokens.
+                    let temp_val = self.index;
+                    self.index = next_index;
                     match character {
-                        ')' => return Some(Token::ParenRight),
-                        '(' => return Some(Token::ParenLeft),
+                        ')' | '(' | '+' | '-' => state = State::Operator,
 
                         '0'..'9' => state = State::Integer,
                         'A'..'Z' | 'a'..'z' => state = State::Identifier,
-                        '"' => state = State::String,
+                        '"' => state = State::StartString,
                         _ => panic!("Unknown character!")
                     }
+                    self.index = temp_val;
                 },
                 State::Integer => {
                     match character {
-                        '0'..'9' => continue,
+                        '0'..'9' => (),
                         '.' => state = State::Float,
-                        _ => return {
-                            Some(Token::Integer(self.get(from_to).parse().unwrap()))
-
-                        }
+                        _ => break
                     } 
                 },
                 State::Float => {
                     match character {
-                        '0'..'9' => continue,
+                        '0'..'9' => (),
                         // Two or more dots!
                         '.' => panic!("Thank you! One dot is enough."),
-                        _ => return Some(Token::Float(self.get(from_to).parse().unwrap()))
+                        _ => break
                     } 
                 },
-                State::String => {
+                State::StartString => {
                     // This means we have reached the second "" which means the string is final.
                     if character == '"'{
-                        return Some(Token::String(&self.input[from_to + '\"'.len_utf8()..self.index - '\"'.len_utf8()]))
+                        state = State::EndString;
+                        //Some(Token::String(&self.input[from_to + '\"'.len_utf8()..self.index - '\"'.len_utf8()]))
                     }
                 },
-                
+                State::EndString => {
+                    break;
+                },
+                State::Operator => {
+                    match &self.input[from_to..next_index] {
+                        "++" => (),
+                        "--" => (),
+                        _ => break
+                    }
+                }
                 _ => todo!()
+            };
+            //println!("Lefutott;");
+            self.index = next_index;
+        };
+
+        // Create token.
+        println!("Final:\nState: {:?} Current: \"{}\"\n", state, &self.input[from_to..self.index]);
+
+        return match state {
+            State::Unknown => Some(Token::Eof),
+            State::Integer => {
+                match &self.input[from_to..self.index].parse::<i32>(){
+                    Ok(num) => Some(Token::Integer(*num)),
+                    Err(e) => panic!("{:?}", e)
+                }
+                
+            },
+           State::Float => {
+                match &self.input[from_to..self.index].parse::<f32>(){
+                    Ok(num) => Some(Token::Float(*num)),
+                    Err(e) => panic!("{:?}", e)
+                }
+                
+            },
+            State::StartString => panic!("Unfinished string!"),
+            State::EndString => Some(Token::String(&self.input[from_to + '\"'.len_utf8()..self.index - '\"'.len_utf8()])),
+            State::Identifier => todo!(),
+            State::Operator => match &self.input[from_to..self.index] {
+                "+" => Some(Token::Plus),
+                "++" => Some(Token::Increment),
+                "-" => Some(Token::Minus),
+                "--" => Some(Token::Decrement),
+                ")" => Some(Token::ParenRight),
+                "(" => Some(Token::ParenLeft),
+                _ => panic!("This should not be possible!")
             }
-        }
-        
+        };
     }
 } 
 
 fn main() {
-    let mut lexer = Lexer::new(" 123.0 ");
+    //println!("{}", &"wasd"[0..1]);
+    let mut lexer = Lexer::new(" (123.0) +- \"asd\"  -- ");
     while let Some(token) = lexer.next() {
         println!("{:?}", token);
         if token == Token::Eof {
@@ -143,9 +179,17 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use crate::{Lexer, Token};
-
     #[test]
-    fn simple_characters(){
+    fn operators() {
+        let mut lexer = Lexer::new("+ - ++ --");
+        assert_eq!(lexer.next(), Some(Token::Plus));
+        assert_eq!(lexer.next(), Some(Token::Minus));
+        assert_eq!(lexer.next(), Some(Token::Increment));
+        assert_eq!(lexer.next(), Some(Token::Decrement));
+        
+    }
+    #[test]
+    fn simple_characters() {
         let mut lexer = Lexer::new("()");
         let vec = vec![lexer.next().unwrap(), lexer.next().unwrap()];
         assert_eq!(vec, [Token::ParenLeft, Token::ParenRight])
@@ -153,13 +197,9 @@ mod tests {
     #[test]
     fn basic_types() {
         let mut lexer = Lexer::new("\"test\" 123 123.0");
-        let string = lexer.next();
-        let integer = lexer.next();
-        let float = lexer.next();
-        let eof = lexer.next();
-        assert_eq!(string, Some(Token::String("test")), "string: {:?}", string);
-        assert_eq!(integer, Some(Token::Integer(123)), "integer: {:?}", integer);
-        assert_eq!(float, Some(Token::Float(123.0)), "float: {:?}", float);
-        assert_eq!(eof, Some(Token::Eof), "eof: {:?}", eof);
+        assert_eq!(lexer.next(), Some(Token::String("test")));
+        assert_eq!(lexer.next(), Some(Token::Integer(123)));
+        assert_eq!(lexer.next(), Some(Token::Float(123.0)));
+        assert_eq!(lexer.next(), Some(Token::Eof));
     }
 }
